6f3b6b13aaf82904d8d06e374ef6e4f53e1f914a
joseph-gitau <57210731+joseph-gitau@users.noreply.github.com>
2023-01-12 18:28:07 +0300

    add layouts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,12 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products') }}
+        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Contact') }}
+        </h2>
     </x-slot>
 
     <div class="py-12">
